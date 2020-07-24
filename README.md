e063dfa882264ec5b85a1bb55ab8c087e8ca2e9c
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-07-24 19:40:30 +0800

    Initial Commit: Library build with default settings.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Touch Sensing on MKL05Zx4 with Freescale/NXP TSS library.
+
+## Important
+For the library to build, the build configuration must have the following
+setttings:
+1. Include TSS_KXX_M0 in C/C++ Build / Tool Settings / Cross ARM C++ Linker / Libraries
+1. Language Standard should be "Toolchain Default (GNU ISO C90)" in
+ C/C++ Build / Tool Settings / Cross ARM C Compiler / Optimization.
+ 
+ For more deailts see: https://community.nxp.com/thread/390760
---
+++ Sources/Events.c
@@ -0,0 +1,153 @@
+/* ###################################################################
+**     Filename    : Events.c
+**     Project     : touch
+**     Processor   : MKL05Z32VLF4
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2020-07-24, 18:41, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.c
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+/* MODULE Events */
+
+#include "Cpu.h"
+#include "Events.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MKL05Z32LF4]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  TSS1_fOnFault (module Events)
+**
+**     Component   :  TSS1 [TSS_Library]
+**     Description :
+**         This callback function is called by TSS after Fault
+**         occurence. This event is enabled always and depends on
+**         selection 'generate code' if the callback is used.
+**         The default CallBack Name is automatically generated with
+**         automatic prefix update by current Component Name. User can
+**         define own name, but then the automatic name update is not
+**         functional.
+**         Option is available from TSS3.0 version.
+**     Parameters  :
+**         NAME            - DESCRIPTION
+**         u8FaultElecNum  - The value defines
+**                           electrode number on which measurement fault
+**                           occured.
+**     Returns     : Nothing
+** ===================================================================
+*/
+void TSS1_fOnFault(byte u8FaultElecNum)
+{
+  #if TSS_USE_PE_COMPONENT
+  /* If large or small capacitor fault  */
+  if(tss_CSSys.Faults.ChargeTimeout || tss_CSSys.Faults.SmallCapacitor)
+  {
+    /* Write your code here ... */
+  }
+  /* If data corruption fault  */
+  if(tss_CSSys.Faults.DataCorruption)
+  {
+    /* Write your code here ... */
+  }
+  /* If small trigger period fault  */
+  if(tss_CSSys.Faults.SmallTriggerPeriod)
+  {
+    /* Write your code here ... */
+  }
+  /* Clear the fault flags */
+  (void) TSS_SetSystemConfig(System_Faults_Register, 0x00);
+
+  (void) u8FaultElecNum;
+  return;
+  #endif
+}
+
+/*
+** ===================================================================
+**     Event       :  TSS1_fOnInit (module Events)
+**
+**     Component   :  TSS1 [TSS_Library]
+**     Description :
+**         This callback function is automatically called during the
+**         TSS Init function execution. The function is intended for
+**         implementation of peripherals initialization. TSS Component
+**         automatically enables clock for all used TSS peripherals in
+**         the internal function TSS_InitDevices which is called by
+**         this callback.
+**         This event is enabled always and depends on selection
+**         'generate code' if the callback is used.
+**         The default CallBack Name is automatically generated with
+**         automatic prefix update by current Component Name. User can
+**         define own name, but then the automatic name update is not
+**         functional.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void TSS1_fOnInit(void)
+{
+  #if TSS_USE_PE_COMPONENT
+    TSS1_InitDevices();
+  #endif
+
+  /* Write your code here ... */
+
+}
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
